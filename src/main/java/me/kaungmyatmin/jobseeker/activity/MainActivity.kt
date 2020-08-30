20d28e23dcfe4ad3b0bef1dcef3cfa349a34684f
KoPhyo <sans461@gmail.com>
2020-08-30 10:29:09 +0630

    search menu

--- src/main/java/me/kaungmyatmin/jobseeker/activity/MainActivity.kt
+++ src/main/java/me/kaungmyatmin/jobseeker/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package me.kaungmyatmin.jobseeker.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import me.kaungmyatmin.jobseeker.JobAdapter
@@ -23,5 +26,37 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    private fun searchJob(q:)
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val menuItem: MenuItem? = menu?.findItem(R.id.search)
+        val actionView: SearchView? = menuItem?.actionView as SearchView?
+        actionView?.let {
+            it.queryHint = "Search ..."
+            it.setOnQueryTextListener (
+                object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextChange(query: String?): Boolean {
+                        searchJob(query)
+                        menuItem?.collapseActionView()
+                        return true
+
+
+                    }
+
+                    override fun onQueryTextSubmit(p0: String?): Boolean {
+                        return true
+
+                    }
+                }
+            )
+            }
+
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
 
 }
